a94c30ad299ccf5adc836ffea12b12d05e937f22
Jim Reinknecht <captainjimscharters@gmail.com>
2017-11-20 10:52:23 -0500

    added json source

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    fs.readFile("./blog/db.json", "utf8", (error, data) => {
+        var db = JSON.parse(data)
+        var dbSlice = db.models.Post.slice(-1)[0];
+        if (dbSlice.source.split('/')[0] === "_discarded") {
+            dbSlice = db.models.Post.slice(-2)[0];
+        }
+        res.render("index", { title: "JamesReinknecht.com", latestBlogEntry: dbSlice })
+    })
 });
 
 module.exports = router;
